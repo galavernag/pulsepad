618492414adf447769a653b8c0f48918a7ac5a4a
Guilherme Galaverna <guigalaverna@gmail.com>
2025-03-18 02:32:14 -0300

    chore: listing soundboards and change searchParams

---
+++ src/app/app/soundboard/_components/soundboard-switcher.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+export function SoundboardSwitcher({
+  soundboards,
+  defaultSoundboardId,
+}: {
+  soundboards: Map<string, { id: string; name: string }>;
+  defaultSoundboardId: string;
+}) {
+  const router = useRouter();
+
+  // Função para lidar com a mudança de soundboard
+  const handleSoundboardChange = (soundboardId: string) => {
+    // Redirecionar para a mesma página, mas com um query param para o soundboard selecionado
+    router.push(`?soundboard=${soundboardId}`);
+  };
+
+  return (
+    <Select
+      defaultValue={defaultSoundboardId}
+      onValueChange={handleSoundboardChange}
+    >
+      <SelectTrigger className="w-[200px]">
+        <SelectValue placeholder="Soundboard" />
+      </SelectTrigger>
+      <SelectContent>
+        {Array.from(soundboards).map(([id, soundboard]) => (
+          <SelectItem key={id} value={id}>
+            {soundboard.name}
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
+  );
+}

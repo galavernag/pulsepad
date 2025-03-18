a984bf160554f969854da10ee12a98318bb08be9
Guilherme Galaverna <guigalaverna@gmail.com>
2025-03-18 18:53:09 -0300

    chore: create action to delete soundboards

---
+++ src/domains/soundboard/actions/delete-soundboard.ts
@@ -0,0 +1,21 @@
+"use server";
+
+import { firestore } from "@/lib/firebase";
+import { deleteDoc, doc } from "firebase/firestore";
+
+export async function deleteSoundboard(id: string) {
+  try {
+    const soundboardRef = doc(firestore, "users", id, "soundboards", id);
+
+    if (!soundboardRef) {
+      return false;
+    }
+
+    await deleteDoc(soundboardRef);
+    return true;
+  } catch (error) {
+    // TODO: Handle error
+    console.error(error);
+    return false;
+  }
+}

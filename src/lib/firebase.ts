2a2fbfa8b7f1f6093dac2fc596c1c3b86459680a
Guilherme Galaverna <guigalaverna@gmail.com>
2025-03-18 12:48:20 -0300

    chore: change access of firebase variables

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,18 +1,21 @@
-import { FirebaseOptions, initializeApp } from "firebase/app";
+import { FirebaseOptions, getApp, getApps, initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getDatabase } from "firebase/database";
 
 import { env } from "./env";
 
 const firebaseConfig: FirebaseOptions = {
-  apiKey: env.FIREBASE_API_KEY,
-  authDomain: env.FIREBASE_AUTH_DOMAIN,
-  projectId: env.FIREBASE_PROJECT_ID,
-  storageBucket: env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: env.FIREBASE_MESSAGING_SENDER_ID,
-  measurementId: env.FIREBASE_MEASUREMENT_ID,
-  appId: env.FIREBASE_APP_ID,
+  apiKey: env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  measurementId: env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  databaseURL: env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  appId: env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = getApps.length > 0 ? getApp() : initializeApp(firebaseConfig);
 export const firestore = getFirestore(app);
+export const db = getDatabase(app);

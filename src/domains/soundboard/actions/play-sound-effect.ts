128208a325829252521b615ca262425c7fdcea79
Guilherme Galaverna <guigalaverna@gmail.com>
2025-03-18 18:47:04 -0300

    chore: create action to play sound on embed

---
+++ src/domains/soundboard/actions/play-sound-effect.ts
@@ -0,0 +1,20 @@
+"use server";
+
+import { db } from "@/lib/firebase";
+import { ref, set } from "firebase/database";
+import { Sound } from "../types";
+
+export async function playSoundEffect(sound: Sound, soundboardId: string) {
+  try {
+    const userRef = ref(db, `embed/${soundboardId}`);
+    await set(userRef, {
+      toPlay: sound,
+      playing: true,
+    });
+    return true;
+  } catch (error) {
+    // TODO: Handle error
+    console.error(error);
+    return false;
+  }
+}

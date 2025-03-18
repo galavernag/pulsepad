5b1202ea3a70dd26605ec34d3ea205b27d4b0159
Guilherme Galaverna <guigalaverna@gmail.com>
2025-03-18 19:46:38 -0300

    fix: change soundboardId to userId

--- src/domains/soundboard/actions/add-sound-effect.ts
+++ src/domains/soundboard/actions/add-sound-effect.ts
@@ -1,15 +1,17 @@
 "use server";
 
 import { firestore } from "@/shared/lib/firebase";
+import { auth } from "@clerk/nextjs/server";
 import { arrayUnion, doc, updateDoc } from "firebase/firestore";
 import { Sound } from "../types";
 
 export async function addSoundEffect(sound: Sound, soundboardId: string) {
+  const { userId } = await auth();
   try {
     const soundboardRef = doc(
       firestore,
       "users",
-      soundboardId,
+      userId!,
       "soundboards",
       soundboardId
     );

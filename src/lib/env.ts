a67f8aaf4974311a69efbb48e3b2e972f5004aae
Guilherme Galaverna <guigalaverna@gmail.com>
2025-03-17 23:50:49 -0300

    chore: add webhook signing

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -3,6 +3,7 @@ import { z } from "zod";
 const envSchema = z.object({
   NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string(),
   CLERK_SECRET_KEY: z.string(),
+  CLERK_SIGNING_SECRET: z.string(),
 
   FIREBASE_API_KEY: z.string(),
   FIREBASE_AUTH_DOMAIN: z.string(),
